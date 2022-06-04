ff710ac45e4ac08b65579f2bf0d2fd5e7c63d27b
nuel07 <yehanganep@gmail.com>
2022-06-04 03:49:25 -0700

    cache tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""implementing an expiring web cache and tracker"""
+
+from typing import Callable
+from functools import wraps
+import redis
+import requests
+
+
+def requests_counter(method: Callable) -> Callable:    
+    """ Counts how many times a request has been made
+    """
+    r = redis.Redis()
+    
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function that counts actual number of requests made"""
+        r.incr(f"count:{url}")
+        cached_ = r.get(f"cached:{url}")
+        if cached:
+            return cached.decode('utf-8')
+        
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+@requests_counter
+def get_page(url: str) -> str:
+    """ return html content for a given url"""
+    resp = requests.get(url)
+    return resp.text
